a5b00dbe25fa874d70859bc0dc9fb00196cfcd86
Mitch <mitchjoelklein@hotmail.com>
2024-11-08 14:05:15 +1100

    addEmployee function

--- db/schema.sql
+++ db/schema.sql
@@ -23,11 +23,8 @@ CREATE TABLE employees (
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
-  manager_id INT,
+  manager_name VARCHAR(30),
   FOREIGN KEY (role_id)
   REFERENCES roles(id)
-  ON DELETE SET NULL,
-  FOREIGN KEY (manager_id)
-  REFERENCES employees(id)
   ON DELETE SET NULL
 );
